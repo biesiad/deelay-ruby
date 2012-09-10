4cbafddf79727860809d945fe2e94cb5241e132e
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-09-10 19:53:06 +0200

    Added sinatra code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in deelay.gemspec
 gemspec
+gem 'sinatra', "~>1.3.3"
--- lib/deelay.rb
+++ lib/deelay.rb
@@ -1,5 +1,22 @@
 require "deelay/version"
+require "sinatra"
+require "uri"
 
 module Deelay
-  # Your code goes here...
+  class App < Sinatra::Application
+    get '/' do
+      "Deelay app is running<BR>TODO: Add info here"
+    end
+
+    get '/:delay' do
+      delay = params[:delay].to_i
+      url = params[:url]
+
+      redirect('http://deelay.me/') if (url =~ URI::regexp).nil?
+
+      sleep delay
+      redirect url
+    end
+
+  end
 end
