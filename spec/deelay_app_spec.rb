fba7672212f5352a0fd8ae09ee3998f46e1811c5
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-11-23 11:37:14 -0800

    Cleaned specs

--- spec/deelay_app_spec.rb
+++ spec/deelay_app_spec.rb
@@ -12,33 +12,30 @@ describe "DeelayApp" do
   end
 
   context "when url invalid" do
-    it "raise error if protocol missing" do
-      expect {
-        get "/10?testurl.com"
-      }.to raise_error
+    it "raise error if query is not a valid url" do
+      expect { get "/10?testurl.com" }.to raise_error
+      expect { get "/10?" }.to raise_error
     end
 
-    it "raise error if protocol missing" do
-      expect {
-        get "/10?/testurl.com"
-      }.to raise_error
+    it "raise error if no query" do
+      expect { get "/10" }.to raise_error
     end
   end
 
   context "when url valid" do
+    it "redirects request to specified url" do
+      get "/10?http://testurl.com"
+      last_response.location.should == "http://testurl.com"
+    end
+
+    it "redirects request" do
+      get "/10?http://testurl.com"
+      last_response.status.should == 302
+    end
+
     it "redirects request with port" do
       get "/10?http://testurl.com:1234"
       last_response.status.should == 302
-    end
-
-    it "redirects request without port" do
-      get "/10?http://testurl.com"
-      last_response.status.should == 302
-    end
-
-    it "redirects to specified url" do
-      get "/10?http://testurl.com"
-      last_response.location.should == "http://testurl.com"
     end
   end
 end
