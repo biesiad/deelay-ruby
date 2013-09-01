11ed03f8c39b2103ac62a715849a1f03b502b43c
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2013-08-31 22:45:14 -0700

    Added parsing splat params

--- lib/deelay/app.rb
+++ lib/deelay/app.rb
@@ -6,20 +6,43 @@ module Deelay
     register Sinatra::Async
 
     aget '/:delay' do
-      resource_url = parse(request.query_string)
+      url = parse_query_string(request.query_string)
+      redirect_to url
+    end
 
-      EM.add_timer(params[:delay].to_i / 1000) do
-        em_async_schedule { redirect resource_url }
-      end
+    aget '/:delay/*' do
+      url = parse_splat(params[:splat].join("/"), request.query_string)
+      redirect_to url
     end
 
     private
 
-    def parse(url)
-      unescaped_url = ::URI.unescape(url)
+    def redirect_to(url, delay = 0)
+      EM.add_timer(delay / 1000) do
+        em_async_schedule { redirect url }
+      end
+    end
 
-      raise "Invalid URL" if (::URI::regexp =~ unescaped_url).nil?
-      return unescaped_url
+    def parse_query_string(raw_url)
+      escaped_url = ::URI.unescape(raw_url)
+      raise ArgumentError, "Missing URL" if request.query_string.empty?
+
+      url = (escaped_url =~ /^http:\/\// ? "" : "http://") + escaped_url
+      return url
+    end
+
+    def parse_splat(raw_url, query_string)
+      raise ArgumentError, "Missing URL" if raw_url.empty?
+
+      escaped_url = ::URI.unescape(raw_url)
+      url = escaped_url.sub(/^http:\//, "http://")
+      url = "http://" + url if url !~ /^http:\/\//
+      url << "?" + request.query_string if !request.query_string.empty?
+      return url
+    end
+
+    def validate(url)
+      raise "Invalid url" if url =~ /(http|https)/
     end
 
     # swaps env when testing
--- test/test_deelay_app.rb
+++ test/test_deelay_app.rb
@@ -13,37 +13,67 @@ class TestDeelayApp < MiniTest::Unit::TestCase
     Deelay::App.new
   end
 
-  def test_redirect_on_valid_request
-      get "/10?http://testurl.com"
+  def test_redirect_query_with_scheme
+      get "/10?http://testurl.com/path"
       em_async_continue
-      assert_equal "http://testurl.com", last_response.location
+      assert_equal "http://testurl.com/path", last_response.location
       assert_equal 302, last_response.status
   end
 
-  def test_redirect_on_valid_request_with_port
-      get "/10?http://testurl.com:1234"
-      em_async_continue
-      assert_equal "http://testurl.com:1234", last_response.location
-      assert_equal 302, last_response.status
+  def test_redirect_query_no_scheme
+    get "/10?testurl.com/path"
+    em_async_continue
+    assert_equal "http://testurl.com/path", last_response.location
+    assert_equal 302, last_response.status
   end
+
+  def test_redirect_query_with_port
+    get "/10?testurl.com:1234/path"
+    em_async_continue
+    assert_equal "http://testurl.com:1234/path", last_response.location
+    assert_equal 302, last_response.status
+  end
+
+
+  def test_redirect_url_with_scheme
+    get "/10/http://testurl.com/path"
+    em_async_continue
+    assert_equal "http://testurl.com/path", last_response.location
+    assert_equal 302, last_response.status
+  end
+
+  def test_redirect_url_no_scheme
+    get "/10/testurl.com/path"
+    em_async_continue
+    assert_equal "http://testurl.com/path", last_response.location
+    assert_equal 302, last_response.status
+  end
+
+  def test_redirect_url_with_port
+    get "/10/testurl.com:1234"
+    em_async_continue
+    assert_equal "http://testurl.com:1234", last_response.location
+    assert_equal 302, last_response.status
+  end
+
+  def test_redirect_url_with_query
+    get "/10/http://testurl.com/path?query=param"
+    em_async_continue
+    assert_equal "http://testurl.com/path?query=param", last_response.location
+    assert_equal 302, last_response.status
+  end
+
 
   def test_no_query
     get "/10"
-    assert_raises(RuntimeError) do
+    assert_raises(ArgumentError) do
       em_async_continue(0.1)
     end
   end
 
   def test_no_link
     get "/10?"
-    assert_raises(RuntimeError) do
-      em_async_continue(0.1)
-    end
-  end
-
-  def test_no_scheme_link
-    get "/10?testurl.com"
-    assert_raises(RuntimeError) do
+    assert_raises(ArgumentError) do
       em_async_continue(0.1)
     end
   end
